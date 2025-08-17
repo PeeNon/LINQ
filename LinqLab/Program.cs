bed74dc93e7c7eb885554b73eb262734368d2053
Nith <sochnithpov.vong@techbodia.com>
2025-08-17 09:26:19 +0700

    Refactor Program.cs to remove commented-out code and add a greeting message

--- LinqLab/Program.cs
+++ LinqLab/Program.cs
@@ -1,46 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Console.WriteLine("Hello, World!");
-
-using LinqLab;
-
-// Console.WriteLine(new FirstAndSingleLab().FirstToGetUserNameEqualToDemo());
-
-// var lab = new FirstAndSingleLab();
-// var result = lab.FirstToGetUserNameEqualToDemo();
-
-// Console.WriteLine($"result: Id = {result.Id}, UserName = {result.UserName}");
-
-// Console.WriteLine($"Items: {string.Join(", ", result.Item)}");
-
-
-// var lab = new FirstAndSingleLab();
-// var result = lab.FirstOrDefaultToGetUserNameEuqalToSkilltree();
-
-// Console.WriteLine($"result: Id = {result?.Id}, UserName = {result?.UserName}");
-
-// Console.WriteLine($"Items: {string.Join(", ", result?.Item ?? [])}");
-
-
-// var lab = new FirstAndSingleLab();
-// var result = lab.FirstToGetUserNameEuqalToSkilltree();
-
-// Console.WriteLine($"result: Id = {result?.Id}, UserName = {result?.UserName}");
-
-// Console.WriteLine($"Items: {string.Join(", ", result?.Item ?? [])}");
-
-// var lab = new FirstAndSingleLab();
-// var result = lab.SingleToGetUserNameEuqalToBill();
-
-// Console.WriteLine($"result: Id = {result?.Id}, UserName = {result?.UserName}");
-
-// Console.WriteLine($"Items: {string.Join(", ", result?.Item ?? [])}");
-
-// var lab = new FirstAndSingleLab();
-// var result = lab.SingleOrDefaultToGetUserNameEuqalToSkillTree();
-
-// Console.WriteLine($"result: Id = {result.Id}, UserName = {result.UserName}");
-// Console.WriteLine($"Items: {string.Join(", ", result.Item ?? [])}");
+Console.WriteLine("Hello, World!");
 
 
 
